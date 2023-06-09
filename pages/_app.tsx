6c45c2252b66ed3f27bb1a8f92e7426e6b3a1d82
Wellington Braga <wellington.braga@ferreiracosta.com.br>
2023-06-09 18:25:33 -0300

    Instalação do Vercel Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
 import type { AppProps } from "next/app";
+import { Analytics } from '@vercel/analytics/react';
+ 
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <GlobalStyle />
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
