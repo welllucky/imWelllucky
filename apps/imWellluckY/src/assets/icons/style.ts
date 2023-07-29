d4f62800b9ffd3942edec399063edeb4010527f8
Wellington Braga <wellington.braga@ferreiracosta.com.br>
2023-07-29 00:58:25 -0300

    Adição de novas configurações do eslint

--- apps/imWellluckY/src/assets/icons/style.ts
+++ apps/imWellluckY/src/assets/icons/style.ts
@@ -3,18 +3,18 @@ import { IconProps } from "../types";
 
 const IconWrapper =
   styled.div <
-  IconProps >
-  `
+    IconProps >
+    `
   display: flex;
   align-items: center;
   justify-content: center;
-  width: ${({ width }) => width || "2.4rem"};
-  height: ${({ height }) => height || "2.4rem"};
+  width: ${({ width }) => width ?? "2.4rem"};
+  height: ${({ height }) => height ?? "2.4rem"};
   & > svg {
-    color: ${({ color }) => color || "#0b0b0a"};
-    height: ${({ height }) => height || "2.4rem"};
-    width: ${({ width }) => width || "2.4rem"};
-    alt: ${({ alt }) => alt || "Icon"};
+    color: ${({ color }) => color ?? "#0b0b0a"};
+    height: ${({ height }) => height ?? "2.4rem"};
+    width: ${({ width }) => width ?? "2.4rem"};
+    alt: ${({ alt }) => alt ?? "Icon"};
     }
 `;
 
