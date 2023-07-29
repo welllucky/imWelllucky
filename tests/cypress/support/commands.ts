b07e3e5dbc46fbd15152c0f68a7920a402337f61
Wellington Braga <wellington.braga@ferreiracosta.com.br>
2023-07-29 00:03:17 -0300

    Retirada dos testes padrões do cypress

---
+++ tests/cypress/support/commands.ts
@@ -0,0 +1,18 @@
+/// <reference types="cypress" />
+
+Cypress.Commands.add("clickOnComponent", (component: string, force = false) => {
+    cy.get(component).click({ force: force, });
+});
+
+Cypress.Commands.add("checkComponentIsVisible", (component: string, isVisible = true) => {
+    isVisible === true ? cy.get(component).should("be.visible") : cy.get(component).should("not.be.visible")
+});
+
+Cypress.Commands.add("pageMustBe", (url: string) => {
+    cy.url().should('eq', url);
+});
+
+Cypress.Commands.add("verifyComponentHref", (component: string, href: string) => {
+    cy.get(component).should("have.attr", "href", href);
+});
+
